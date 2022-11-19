f58e1390993ec0e0ddea05c9c2790968e7dde315
mohiuddinshahrukh <mohiuddinshahrukh@gmail.com>
2022-11-20 02:12:11 +0500

    workingonvenuecards

--- src/components/featuredVenuesSection/FeaturedVenuesCarousel.jsx
+++ src/components/featuredVenuesSection/FeaturedVenuesCarousel.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Carousel } from "@mantine/carousel";
+import { Card, Group, Image, Paper, Text } from "@mantine/core";
+import { IconStar } from "@tabler/icons";
 const fetchVenuesMethod = async () => {
   try {
     const apiResponse = await axios.get(
@@ -7,6 +10,7 @@ const fetchVenuesMethod = async () => {
     );
     if (apiResponse.data.status === "success") {
       console.log("API RESPONSE SUCCESS: ", apiResponse);
+      return apiResponse.data.venueData;
     } else if (apiResponse.data.status === "error") {
       console.log("API RESPONSE SUCCESS: ", apiResponse);
     } else {
@@ -18,10 +22,61 @@ const fetchVenuesMethod = async () => {
 };
 
 const FeaturedVenuesCarousel = () => {
+  const [landingPageVenues, setLandingPageVenues] = useState([]);
   useEffect(() => {
-    fetchVenuesMethod();
-  });
-  return <div></div>;
+    fetchVenuesMethod().then(setLandingPageVenues);
+
+    console.count("USE EFFECT: ");
+
+    return () => {
+      console.count("CLEAN UP: ");
+    };
+  }, []);
+
+  return (
+    <Carousel
+      slideSize={"25% "}
+      slideGap={"md"}
+      align={"start"}
+      slidesToScroll={4}
+      withControls={false}
+      withIndicators
+    >
+      {landingPageVenues.map((venue, index) => {
+        return (
+          <Carousel.Slide key={index}>
+            <Card shadow={"sm"} style={{ width: "302px" }} withBorder>
+              <Card.Section style={{ height: "201px" }}>
+                <Image
+                  height={"201px"}
+                  width={"100%"}
+                  fit={"cover"}
+                  src={venue.coverImage}
+                />
+              </Card.Section>
+              <Card.Section>
+                <Paper p={"lg"}>
+                  <Text lineClamp={1} weight={500} size={"lg"}>
+                    {venue.venueName}
+                  </Text>
+
+                  <Group spacing={3}>
+                    <IconStar size={20} stroke={0} fill={"#EDB100"} />
+                    <Text weight={500} size={"sm"}>
+                      {venue.rating.toFixed(1)}
+                    </Text>
+                    <Text color={"dimmed"} size={"sm"}>
+                      ({venue.ratingCount})
+                    </Text>
+                  </Group>
+                </Paper>
+              </Card.Section>
+            </Card>
+          </Carousel.Slide>
+        );
+      })}
+    </Carousel>
+  );
 };
 
 export default FeaturedVenuesCarousel;

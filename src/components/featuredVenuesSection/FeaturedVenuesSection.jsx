0d81155a1ec53ec76cecae56ab79d492b783f522
mohiuddinshahrukh <mohiuddinshahrukh@gmail.com>
2022-11-19 21:15:14 +0500

    commitForMap

--- src/components/featuredVenuesSection/FeaturedVenuesSection.jsx
+++ src/components/featuredVenuesSection/FeaturedVenuesSection.jsx
@@ -1,7 +1,26 @@
+import { Anchor, Button, Container, Group } from "@mantine/core";
+import { IconArrowRight } from "@tabler/icons";
 import React from "react";
+import { Link } from "react-router-dom";
+import FeaturedVenuesCarousel from "./FeaturedVenuesCarousel";
 
 const FeaturedVenuesSection = () => {
-  return <div></div>;
+  return (
+    <Container size={"xl"} mt="xl">
+      <Group position="center">
+        <Anchor component={Link} to="#" variant="text" size={"1.5rem"}>
+          Wedding Venues
+        </Anchor>
+      </Group>
+      <Group position="right">
+        <Button color={"dark"} variant="outline" rightIcon={<IconArrowRight />}>
+          View All Venues
+        </Button>
+      </Group>
+
+      <FeaturedVenuesCarousel />
+    </Container>
+  );
 };
 
 export default FeaturedVenuesSection;

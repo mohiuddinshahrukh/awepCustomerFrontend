5fda087f2c6568787e89eb26a9dee31c37fab616
mohiuddinshahrukh <mohiuddinshahrukh@gmail.com>
2022-11-20 20:37:09 +0500

    VenueCardsDone

--- src/components/featuredVenuesSection/FeaturedVenuesCarousel.jsx
+++ src/components/featuredVenuesSection/FeaturedVenuesCarousel.jsx
@@ -2,7 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Carousel } from "@mantine/carousel";
 import { Card, Group, Image, Paper, Text } from "@mantine/core";
-import { IconStar } from "@tabler/icons";
+import {
+  IconBuildingArch,
+  IconBuildingFortress,
+  IconCash,
+  IconStar,
+  IconUsers,
+} from "@tabler/icons";
+import { Link } from "react-router-dom";
 const fetchVenuesMethod = async () => {
   try {
     const apiResponse = await axios.get(
@@ -45,7 +52,17 @@ const FeaturedVenuesCarousel = () => {
       {landingPageVenues.map((venue, index) => {
         return (
           <Carousel.Slide key={index}>
-            <Card shadow={"sm"} style={{ width: "302px" }} withBorder>
+            <Card
+              sx={{
+                ":hover": { boxShadow: "1px 1px 3px rgba(0, 0, 0, .25)" },
+                transition: "0.3s",
+              }}
+              shadow={"sm"}
+              component={Link}
+              to={"#"}
+              style={{ width: "302px" }}
+              withBorder
+            >
               <Card.Section style={{ height: "201px" }}>
                 <Image
                   height={"201px"}
@@ -60,13 +77,79 @@ const FeaturedVenuesCarousel = () => {
                     {venue.venueName}
                   </Text>
 
-                  <Group spacing={3}>
-                    <IconStar size={20} stroke={0} fill={"#EDB100"} />
+                  <Group spacing={3} noWrap>
+                    <IconStar
+                      size={20}
+                      style={{ flexShrink: 0 }}
+                      stroke={0}
+                      fill={"#EDB100"}
+                    />
                     <Text weight={500} size={"sm"}>
                       {venue.rating.toFixed(1)}
                     </Text>
                     <Text color={"dimmed"} size={"sm"}>
                       ({venue.ratingCount})
+                    </Text>
+                    <Text lineClamp={1}>{venue.venueAddress}</Text>
+                  </Group>
+
+                  <Group noWrap spacing={"lg"} align={"center"}>
+                    <Group spacing={3} noWrap align={"center"}>
+                      <IconBuildingFortress size={20} stroke={1.5} />
+                      <Text>
+                        {venue.subVenues.length === 1
+                          ? venue.subVenues.length + " Subvenue"
+                          : venue.subVenues.length + " Subvenues"}
+                      </Text>
+                    </Group>
+                    <Group noWrap spacing={3} align={"center"}>
+                      {" "}
+                      <IconUsers size={18} stroke={1.5} />
+                      <Text>
+                        {Math.min
+                          .apply(
+                            Math,
+                            venue.subVenues.map((subvenue) => {
+                              return subvenue.subVenueMinCapacity;
+                            })
+                          )
+                          .toLocaleString()}
+                      </Text>
+                      to
+                      <Text>
+                        {Math.max
+                          .apply(
+                            Math,
+                            venue.subVenues.map((subvenue) => {
+                              return subvenue.subVenueCapacity;
+                            })
+                          )
+                          .toLocaleString()}
+                      </Text>
+                    </Group>
+                  </Group>
+
+                  <Group noWrap align={"center"} spacing={3}>
+                    <IconCash stroke={1.5} size={20} />
+                    <Text>
+                      from Rs.{" "}
+                      {venue.menus.length !== 0
+                        ? Math.max
+                            .apply(
+                              Math,
+                              venue.menus.map((menu) => {
+                                return menu.price;
+                              })
+                            )
+                            .toLocaleString() + " (Per Head)"
+                        : Math.max
+                            .apply(
+                              Math,
+                              venue.subVenues.map((subvenue) => {
+                                return subvenue.subVenueBookingCharges;
+                              })
+                            )
+                            .toLocaleString() + " (Per Event)"}
                     </Text>
                   </Group>
                 </Paper>

cc8b0ba5a531545b442b755baeb16781851ba0f3
mohiuddinshahrukh <mohiuddinshahrukh@gmail.com>
2022-11-22 15:24:27 +0500

    aliCodedsomeStuff&ifixedIt

--- src/components/allVenuesPage/AllVenuesVenueCard.jsx
+++ src/components/allVenuesPage/AllVenuesVenueCard.jsx
@@ -1,4 +1,12 @@
-import { Card, Group, Image, Paper, SimpleGrid, Text } from "@mantine/core";
+import {
+  Anchor,
+  Card,
+  Group,
+  Image,
+  Paper,
+  SimpleGrid,
+  Text,
+} from "@mantine/core";
 import {
   IconBuildingFortress,
   IconCash,
@@ -35,92 +43,95 @@ const AllVenuesVenueCard = ({ allVenues }) => {
         <Card.Section style={{ height: "201px" }}>
           <AllVenuesVenueCardCarousel venueImages={venue.images} />
         </Card.Section>
-        <Card.Section
-          style={{ height: "154px" }}
-          component={Link}
-          to={`/specificVenue${venue._id}`}
-        >
-          <Paper p={"lg"}>
-            <Text lineClamp={1} weight={500} size={"lg"}>
-              {venue?.venueName}
-            </Text>
-
-            <Group spacing={3} noWrap>
-              <IconStar
-                size={20}
-                style={{ flexShrink: 0 }}
-                stroke={0}
-                fill={"#EDB100"}
-              />
-              <Text weight={500} size={"sm"}>
-                {venue?.rating.toFixed(1)}
+        <Card.Section style={{ height: "154px" }}>
+          <Anchor
+            variant="text"
+            component={Link}
+            to={`/specificVenue${venue._id}`}
+          >
+            {" "}
+            <Paper p={"lg"}>
+              <Text lineClamp={1} weight={500} size={"lg"}>
+                {venue?.venueName}
               </Text>
-              <Text color={"dimmed"} size={"sm"}>
-                ({venue?.ratingCount})
-              </Text>
-              <Text lineClamp={1}>{venue?.venueAddress}</Text>
-            </Group>
 
-            <Group noWrap spacing={"lg"} align={"center"}>
-              <Group spacing={3} noWrap align={"center"}>
-                <IconBuildingFortress size={20} stroke={1.5} />
-                <Text>
-                  {venue?.subVenues.length === 1
-                    ? venue?.subVenues.length + " Subvenue"
-                    : venue?.subVenues.length + " Subvenues"}
+              <Group spacing={3} noWrap>
+                <IconStar
+                  size={20}
+                  style={{ flexShrink: 0 }}
+                  stroke={0}
+                  fill={"#EDB100"}
+                />
+                <Text weight={500} size={"sm"}>
+                  {venue?.rating.toFixed(1)}
                 </Text>
+                <Text color={"dimmed"} size={"sm"}>
+                  ({venue?.ratingCount})
+                </Text>
+                <Text lineClamp={1}>{venue?.venueAddress}</Text>
               </Group>
-              <Group noWrap spacing={3} align={"center"}>
-                {" "}
-                <IconUsers size={18} stroke={1.5} />
-                <Text>
-                  {Math.min
-                    .apply(
-                      Math,
-                      venue?.subVenues?.map((subvenue) => {
-                        return subvenue?.subVenueMinCapacity;
-                      })
-                    )
-                    .toLocaleString()}
-                </Text>
-                to
-                <Text>
-                  {Math.max
-                    .apply(
-                      Math,
-                      venue?.subVenues?.map((subvenue) => {
-                        return subvenue?.subVenueCapacity;
-                      })
-                    )
-                    .toLocaleString()}
-                </Text>
-              </Group>
-            </Group>
 
-            <Group noWrap align={"center"} spacing={3}>
-              <IconCash stroke={1.5} size={20} />
-              <Text>
-                from Rs.{" "}
-                {venue?.menus.length !== 0
-                  ? Math.max
-                      .apply(
-                        Math,
-                        venue?.menus?.map((menu) => {
-                          return menu.price;
-                        })
-                      )
-                      .toLocaleString() + " (Per Head)"
-                  : Math.max
+              <Group noWrap spacing={"lg"} align={"center"}>
+                <Group spacing={3} noWrap align={"center"}>
+                  <IconBuildingFortress size={20} stroke={1.5} />
+                  <Text>
+                    {venue?.subVenues.length === 1
+                      ? venue?.subVenues.length + " Subvenue"
+                      : venue?.subVenues.length + " Subvenues"}
+                  </Text>
+                </Group>
+                <Group noWrap spacing={3} align={"center"}>
+                  {" "}
+                  <IconUsers size={18} stroke={1.5} />
+                  <Text>
+                    {Math.min
                       .apply(
                         Math,
                         venue?.subVenues?.map((subvenue) => {
-                          return subvenue?.subVenueBookingCharges;
+                          return subvenue?.subVenueMinCapacity;
                         })
                       )
-                      .toLocaleString() + " (Per Event)"}
-              </Text>
-            </Group>
-          </Paper>
+                      .toLocaleString()}
+                  </Text>
+                  to
+                  <Text>
+                    {Math.max
+                      .apply(
+                        Math,
+                        venue?.subVenues?.map((subvenue) => {
+                          return subvenue?.subVenueCapacity;
+                        })
+                      )
+                      .toLocaleString()}
+                  </Text>
+                </Group>
+              </Group>
+
+              <Group noWrap align={"center"} spacing={3}>
+                <IconCash stroke={1.5} size={20} />
+                <Text>
+                  from Rs.{" "}
+                  {venue?.menus.length !== 0
+                    ? Math.max
+                        .apply(
+                          Math,
+                          venue?.menus?.map((menu) => {
+                            return menu.price;
+                          })
+                        )
+                        .toLocaleString() + " (Per Head)"
+                    : Math.max
+                        .apply(
+                          Math,
+                          venue?.subVenues?.map((subvenue) => {
+                            return subvenue?.subVenueBookingCharges;
+                          })
+                        )
+                        .toLocaleString() + " (Per Event)"}
+                </Text>
+              </Group>
+            </Paper>
+          </Anchor>
         </Card.Section>
       </Card>
     );

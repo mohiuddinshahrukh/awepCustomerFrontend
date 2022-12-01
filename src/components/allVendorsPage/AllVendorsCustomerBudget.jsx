32806909c4ce9af6902b3a1cc5d249b4f2caeef0
hasanzaidi1996 <hasanzaidi2896@gmail.com>
2022-12-01 14:52:28 +0500

    working filtering

--- src/components/allVendorsPage/AllVendorsCustomerBudget.jsx
+++ src/components/allVendorsPage/AllVendorsCustomerBudget.jsx
@@ -1,5 +1,6 @@
 import {
   Accordion,
+  Divider,
   Group,
   NumberInput,
   RangeSlider,
@@ -23,53 +24,58 @@ const AllVendorsCustomerBudget = ({
   }, [minPrice, maxPrice]);
 
   return (
-    <Accordion chevronPosition="left" defaultValue="menuPrice">
-      <Accordion.Item value="menuPrice">
-        <Accordion.Control>Package Budget</Accordion.Control>
-        <Accordion.Panel>
-          <RangeSlider
-            thumbSize={14}
-            max={maxPrice}
-            min={minPrice}
-            // defaultValue={[20, 80]}
-            value={rangeValue}
-            onChange={(value) => {
-              setRangeValue(value);
-              setMinPriceFilter(value[0]);
-              setMaxPriceFilter(value[1]);
-            }}
-          />
-        </Accordion.Panel>
-        <Accordion.Panel>
-          <Group position="apart" noWrap>
-            <NumberInput
-              icon={<Text>Rs.</Text>}
-              label="From"
-              min={minPrice}
-              max={maxPrice}
-              hideControls
-              value={rangeValue[0]}
-              onChange={(e) => {
-                setMinPriceFilter(e);
-                setRangeValue([e, rangeValue[1]]);
-              }}
-            />
-            <NumberInput
-              hideControls
-              icon={<Text>Rs.</Text>}
-              label="To"
-              min={minPrice}
-              max={maxPrice}
-              value={rangeValue[1]}
-              onChange={(e) => {
-                setMaxPriceFilter(e);
-                setRangeValue([rangeValue[0], e]);
-              }}
-            />
-          </Group>
-        </Accordion.Panel>
-      </Accordion.Item>
-    </Accordion>
+    // <Accordion chevronPosition="left" defaultValue="menuPrice">
+    //   <Accordion.Item value="menuPrice">
+    //     <Accordion.Control>Price</Accordion.Control>
+    //     <Accordion.Panel>
+    <>
+      <Divider />
+
+      <RangeSlider
+        thumbSize={14}
+        max={maxPrice}
+        min={minPrice}
+        // defaultValue={[20, 80]}
+        value={rangeValue}
+        onChange={(value) => {
+          setRangeValue(value);
+          setMinPriceFilter(value[0]);
+          setMaxPriceFilter(value[1]);
+        }}
+      />
+
+      <Group position="apart" noWrap>
+        <NumberInput
+          icon={<Text>Rs.</Text>}
+          label="From"
+          min={minPrice}
+          max={maxPrice}
+          hideControls
+          value={rangeValue[0]}
+          onChange={(e) => {
+            setMinPriceFilter(e);
+            setRangeValue([e, rangeValue[1]]);
+          }}
+        />
+        <NumberInput
+          hideControls
+          icon={<Text>Rs.</Text>}
+          label="To"
+          min={minPrice}
+          max={maxPrice}
+          value={rangeValue[1]}
+          onChange={(e) => {
+            setMaxPriceFilter(e);
+            setRangeValue([rangeValue[0], e]);
+          }}
+        />
+      </Group>
+      <Divider />
+
+    </>
+    //     </Accordion.Panel>
+    //   </Accordion.Item>
+    // </Accordion>
   );
 };
 

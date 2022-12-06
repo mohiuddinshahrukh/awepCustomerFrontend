137b6638430ec8baacbb51190a65eeb2ed482b66
mohiuddinshahrukh <mohiuddinshahrukh@gmail.com>
2022-12-06 11:51:38 +0500

    userProfileSettingDone

--- src/components/navigation/topNavbar/TopNavbarUserProfileIcon.jsx
+++ src/components/navigation/topNavbar/TopNavbarUserProfileIcon.jsx
@@ -1,8 +1,13 @@
 import { ActionIcon, Anchor, Avatar, Group, Menu, Text } from "@mantine/core";
 import {
+  IconDashboard,
+  IconEdit,
+  IconLayout,
+  IconLayoutGrid,
   IconLogout,
   IconPhoneCheck,
   IconPhoneX,
+  IconSettings,
   IconUserCheck,
   IconUserX,
 } from "@tabler/icons";
@@ -20,29 +25,79 @@ const TopNavbarUserProfileIcon = ({ setSignedIn }) => {
   );
 
   return (
-    <Menu withArrow width={350} position="bottom" transition="pop">
+    <Menu withArrow width={275} position="bottom" transition="pop">
       <Menu.Target>
         <ActionIcon>
           <Avatar size={43} src={loggedInUserData.profileImage} />
         </ActionIcon>
       </Menu.Target>
       <Menu.Dropdown>
-        {[
-          { title: "Dashboard", path: "dashboard" },
-          { title: "Profile", path: "dashboard/profile" },
-        ].map((menuOption, index) => {
-          return (
-            <Anchor
-              key={index}
-              component={Link}
-              variant={"text"}
-              to={menuOption.path}
-            >
-              <Menu.Item key={index}>{menuOption.title}</Menu.Item>
-            </Anchor>
-          );
-        })}
+        <Menu.Item>
+          <Group position="center">
+            <Avatar
+              size={"lg"}
+              radius="xl"
+              src={loggedInUserData.profileImage}
+            />
+            <div style={{ textAlign: "center" }}>
+              <Text weight={500}>{loggedInUserData.name}</Text>
+              <Text size="xs" color="dimmed">
+                {loggedInUserData.email}
+              </Text>
+              <Text size="xs" color="dimmed">
+                {loggedInUserData.userType}
+              </Text>
+            </div>
+          </Group>
+        </Menu.Item>
+        <Menu.Divider style={{}} />
         <Menu.Item
+          onClick={() => {
+            navigate("/dashboard");
+          }}
+          icon={<IconLayoutGrid color="#e60084" />}
+        >
+          Dashboard
+        </Menu.Item>
+        {/*<Menu.Divider />
+        <Menu.Label>Verification Status</Menu.Label>
+        <Menu.Item closeMenuOnClick={false}>
+          {loggedInUserData.isEmailVerified === true ? (
+            <Group>
+              <IconUserCheck color="green" />
+              <Text>Email is verified</Text>
+            </Group>
+          ) : (
+            <Group>
+              <IconUserX color="red" />
+              <Text>Email is unverified</Text>
+            </Group>
+          )}
+        </Menu.Item>
+        <Menu.Item closeMenuOnClick={false}>
+          {loggedInUserData.isPhoneVerified === true ? (
+            <Group>
+              <IconPhoneCheck color="green" />
+              <Text>Phone is verified</Text>
+            </Group>
+          ) : (
+            <Group>
+              <IconPhoneX color="red" />
+              <Text>Phone is unverified</Text>
+            </Group>
+          )}
+        </Menu.Item>*/}
+
+        <Menu.Item
+          icon={<IconSettings color="orange" />}
+          onClick={() => {
+            navigate("/dashboard/profile");
+          }}
+        >
+          Settings
+        </Menu.Item>
+        <Menu.Item
+          icon={<IconLogout color="red" />}
           onClick={() => {
             localStorage.removeItem("customerData");
             localStorage.removeItem("customerToken");
@@ -64,6 +119,7 @@ const TopNavbarUserProfileIcon = ({ setSignedIn }) => {
             ) {
               console.log("CURRENT LOCATION AND PATH MATCHED");
               navigate({ pathname: "/" });
+              setSignedIn(false);
             } else {
               setSignedIn(false);
             }
@@ -77,103 +133,19 @@ const TopNavbarUserProfileIcon = ({ setSignedIn }) => {
 };
 
 export default TopNavbarUserProfileIcon;
-//
 
-// <Menu withArrow width={350} position="bottom" transition="pop">
-// <Menu.Target>
-//   <ActionIcon>
-//     <Avatar radius="xl" src={loggedInUserData.profileImage} />
-//   </ActionIcon>
-// </Menu.Target>
-// <Menu.Dropdown>
-//   <Menu.Item onClick={() => navigate("/")}>
-//     <Group>
-//       <div>
-//         <Text weight={500}>{loggedInUserData.name}</Text>
-//         <Text size="xs" color="dimmed">
-//           {loggedInUserData.email}
-//         </Text>
-//         <Text size="xs" color="dimmed">
-//           {loggedInUserData.userType}
-//         </Text>
-//       </div>
-//     </Group>
-//   </Menu.Item>
-//   <Menu.Divider />
-//   <Menu.Label>Verification Status</Menu.Label>
-//   <Menu.Item closeMenuOnClick={false}>
-//     {loggedInUserData.isEmailVerified === true ? (
-//       <Group>
-//         <IconUserCheck color="green" />
-//         <Text>Email is verified</Text>
-//       </Group>
-//     ) : (
-//       <Group>
-//         <IconUserX color="red" />
-//         <Text>Email is unverified</Text>
-//       </Group>
-//     )}
-//   </Menu.Item>
-//   <Menu.Item closeMenuOnClick={false}>
-//     {loggedInUserData.isPhoneVerified === true ? (
-//       <Group>
-//         <IconPhoneCheck color="green" />
-//         <Text>Phone is verified</Text>
-//       </Group>
-//     ) : (
-//       <Group>
-//         <IconPhoneX color="red" />
-//         <Text>Phone is unverified</Text>
-//       </Group>
-//     )}
-//   </Menu.Item>
-//   <Menu.Divider />
-//   <Menu.Label>Settings</Menu.Label>
-//   <Menu.Item
-//   // icon={<Edit color="green" />}
-//   // onClick={() => {
-//   //   navigate("/editProfile", {
-//   //     state: {
-//   //       ID: loggedInUserData.id,
-//   //       NAME: loggedInUserData.name,
-//   //       EMAIL: loggedInUserData.email,
-//   //       USERTYPE: loggedInUserData.userType,
-//   //       PROFILEIMAGE: loggedInUserData.profileImage,
-//   //       ISEMAILVERIFIED: loggedInUserData.isEmailVerified,
-//   //       ISPHONEVERIFIED: loggedInUserData.isPhoneVerified,
-//   //       CNIC: loggedInUserData.CNIC,
-//   //       PHONE: loggedInUserData.phone,
-//   //       TOKEN: loggedInUserData.token,
-//   //     },
-//   //   });
-//   // }}
-//   >
-//     Edit Profile
-//   </Menu.Item>
-//   {/* As No More Settings, it has been commented. */}
-//   {/*
-// <Menu.Item
-// icon={<IconSettings />}
-// onClick={() => navigate("/settings")}
-// >
-// Account settings
-// </Menu.Item>
-// <Menu.Divider /> */}
-//   <Menu.Label>Danger zone</Menu.Label>
-//   <Menu.Item
-//     icon={<IconLogout />}
-//     onClick={() => {
-//       // dispatch(logout());
-//       // dispatch(forgetToken());
-//       localStorage.setItem("userToken", "");
-//       localStorage.setItem("customerData", "");
-//       localStorage.clear();
-//       navigate("/signin");
-//     }}
-//   >
-//     Logout
-//   </Menu.Item>
-// </Menu.Dropdown>
-// </Menu>
-
-//
+// {[
+//   { title: "Dashboard", path: "dashboard" },
+//   { title: "Profile", path: "dashboard/profile" },
+// ].map((menuOption, index) => {
+//   return (
+//     <Anchor
+//       key={index}
+//       component={Link}
+//       variant={"text"}
+//       to={menuOption.path}
+//     >
+//       <Menu.Item key={index}>{menuOption.title}</Menu.Item>
+//     </Anchor>
+//   );
+// })}

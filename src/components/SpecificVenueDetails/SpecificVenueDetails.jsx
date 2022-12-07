ec08e80d84ce8fc54706ea07eba0c12f9815c8ba
hasanzaidi1996 <hasanzaidi2896@gmail.com>
2022-12-07 18:54:08 +0500

    vendor specific page tabs

--- src/components/SpecificVenueDetails/SpecificVenueDetails.jsx
+++ src/components/SpecificVenueDetails/SpecificVenueDetails.jsx
@@ -38,6 +38,7 @@ import {
   IconPhone,
   IconMail,
   IconMessage2,
+  IconVideo,
 } from "@tabler/icons";
 import SignIn from "../userProfiling/SignIn";
 import SignUp from "../userProfiling/SignUp";
@@ -265,13 +266,13 @@ const SpecificVenueDetails = () => {
               // }}
             >
               <Tabs.List>
-                <Tabs.Tab value="photos" icon={<IconMessageCircle size={14} />}>
+                <Tabs.Tab value="photos" icon={<IconPhoto size={14} />}>
                   Photos
                 </Tabs.Tab>
 
                 <Tabs.Tab
                   value="videos"
-                  icon={<IconPhoto size={14} />}
+                  icon={<IconVideo size={14} />}
                   hidden={
                     venueDetails?.videos?.length === 0 ||
                     venueDetails?.videos == undefined
